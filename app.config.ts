f5d91fa7c34398c4b2ed90c15b05947b64051748
Nico Dinata <nicodnt@icloud.com>
2024-04-02 19:50:24 +1100

    merge app config

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,8 @@ import { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-
-  // TODO: copy over the `name` and `slug` from `app.json`, and remove that file
-  // afterwards.
-  name: config.name!,
-  slug: config.slug!,
-
+  name: "navy",
+  slug: "navy",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./src/assets/images/icon.png",
