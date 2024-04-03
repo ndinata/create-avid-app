c6be20e8866b4edf178a147bc190b49e95a3ecb4
Nico Dinata <nicodnt@icloud.com>
2024-04-03 23:25:16 +1100

    add sample screen in home stack

--- src/app/(app)/(home)/_layout.tsx
+++ src/app/(app)/(home)/_layout.tsx
@@ -4,6 +4,7 @@ export default function HomeTabLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="home/detail" options={{ title: "Details" }} />
     </Stack>
   );
 }
--- src/app/(app)/(home)/index.tsx
+++ src/app/(app)/(home)/index.tsx
@@ -2,6 +2,10 @@ import { router } from "expo-router";
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen() {
+  const goToDetails = () => {
+    router.push("/home/detail");
+  };
+
   const tryGoBack = () => {
     if (router.canGoBack()) {
       console.error("Should not be able to go back from the tab index");
@@ -19,9 +23,25 @@ export default function HomeScreen() {
       <Text style={styles.title}>Home</Text>
 
       <TouchableOpacity
-        onPress={tryGoBack}
+        onPress={goToDetails}
         style={{
           marginVertical: 24,
+          backgroundColor: "white",
+          borderRadius: 6,
+          borderWidth: 1,
+          borderColor: "grey",
+          paddingHorizontal: 32,
+          paddingVertical: 12,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontSize: 16, textAlign: "center" }}>Go to details</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={tryGoBack}
+        style={{
           backgroundColor: "white",
           borderRadius: 6,
           borderWidth: 1,
