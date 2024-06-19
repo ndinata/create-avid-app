fe94db0ad87f0c233e043ff4a5126e667738486a
Nico Dinata <nicodnt@icloud.com>
2024-06-19 15:41:40 +1000

    upgrade expo to sdk 51

--- app.config.ts
+++ app.config.ts
@@ -35,7 +35,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     output: "static",
     favicon: "./src/assets/images/favicon.png",
   },
-  plugins: ["expo-router"],
+  plugins: ["expo-router", "expo-font"],
   experiments: {
     typedRoutes: true,
   },
